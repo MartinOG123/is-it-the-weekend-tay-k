0e3000280a920518fef590f00d0c3f0d9085fe90
Martin Martinez <martinm50330@students.laalliance.org>
2017-11-17 20:27:27 +0000

    I'm done with my lab.

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var answer = 'NO'
+if(.length < 0) 
+
+
+ function setup(){
+  createCanvas(windowWidth, windowHeight)
+}
+
+function draw(){
+    background('dodgerblue')
+    textSize(32)
+    fill('white')
+    text('Is it the weekend? ' + answer, 40, 40)
+}
